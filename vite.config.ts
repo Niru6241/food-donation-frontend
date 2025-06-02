48a09fc19bd09bdaf360faab6ba939fdfa4e293f
Niranjan <niru18.gitam@gmail.com>
2025-06-01 18:57:04 -0600

    Add support to remove logs from prod

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      }
+    }
+  }
 })
